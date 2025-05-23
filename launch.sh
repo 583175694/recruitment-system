f2aad3cea6f07f0b3bcf7214eb0c8371ed64221a
583175694 <10918338+w583175694@user.noreply.gitee.com>
2025-05-24 01:39:36 +0800

    Remove ecosystem configuration and local Nginx setup; add launch and local scripts for client and server build and deployment.

---
+++ launch.sh
@@ -0,0 +1,24 @@
+git pull
+
+# 解压客户端文件
+cd frontend
+rm -rf dist node_modules package.json package-lock.json
+mv ../client-dist.tar.gz .
+tar -xzf client-dist.tar.gz
+
+# 重启客户端
+pm2 delete recruitment-client
+pm2 start npm --name recruitment-client -- start
+rm -rf client-dist.tar.gz
+
+# 服务端
+cd ..
+# 进入服务端目录
+cd backend
+rm -rf dist node_modules package.json package-lock.json
+mv ../server-dist.tar.gz .
+tar -xzf server-dist.tar.gz
+
+pm2 delete recruitment-server
+pm2 start npm --name recruitment-server -- start
+rm -rf server-dist.tar.gz
---
+++ local.sh
@@ -0,0 +1,16 @@
+# 客户端构建
+cd frontend
+npm run build
+
+# 客户端打包
+COPYFILE_DISABLE=1 tar --no-xattrs -czf client-dist.tar.gz dist node_modules package.json package-lock.json
+# 服务端构建  
+cd ../backend
+npm run build
+
+# # 服务端打包
+cd ../server  
+COPYFILE_DISABLE=1 tar --no-xattrs -czf server-dist.tar.gz dist node_modules package.json package-lock.json
+cd ..
+scp ./frontend/client-dist.tar.gz ubuntu@175.178.23.83:/www/recruitment-system/
+scp ./backend/server-dist.tar.gz ubuntu@175.178.23.83:/www/recruitment-system/
